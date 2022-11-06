b84421c5c4b0d39970dd1af99e29ee64c4075d33
Yifang Tsai <tsaiyftw@gmail.com>
2022-11-06 12:49:42 +0800

    feat: reduce the redundant lines

--- midterm/midterm_v3.py
+++ midterm/midterm_v3.py
@@ -1,10 +1,10 @@
 from myarray import myArray2D, linearSet, binarySet, SparseMatrix
 
 
-# method #2 to read entry element from txt file and store those elements in list of list
-def readfile(input_file):
+# read entry element from txt file and store those elements in a list of list
+def readfile(input_file_pathway):
     big_lst = []
-    with open(input_file) as f:
+    with open(input_file_pathway) as f:
         lines = f.readlines()
         for i in range(len(lines)):
             entry_row = lines[i].split()
@@ -12,22 +12,18 @@ def readfile(input_file):
     return big_lst
 
 
-data = readfile("mA.txt")
-
-# load data to matrix
+# load the file data in the list of list to a 2D matrix (i.e. row-major)
 def loadMatrix(data):
     numRow = len(data)
     numCol = len(data[0])
     newMatrix = myArray2D(numRow, numCol)
-    i = 0
-    for row in data:
-        for j in range(len(row)):
-            newMatrix[i, j] = row[j]
-        i += 1
+    for i in range(numRow):
+        for j in range(numCol):
+            newMatrix[i, j] = data[i][j]
     return newMatrix
 
 
-# create a traspose matrix from origin matrix and store in 2D array ADT
+# create a traspose matrix of an origin matrix and store in 2D array ADT (i.e. column-major)
 def transpose(data):
     numRow = len(data)
     numCol = len(data[0])
@@ -46,7 +42,7 @@ def findValueRowMajor(data, index):
     return matrixA[i, j]
 
 
-# Given an index number and return its value in a 2D row-column array
+# Given an index number and return its value in a 2D column-major array
 def findValueColMajor(data, index):
     matrixT = transpose(data)
     i = index // len(data)
@@ -54,11 +50,10 @@ def findValueColMajor(data, index):
     return matrixT[i, j]
 
 
+# multiply two matrix and return the result as a 2D matrix
 def multiply(matrixA, matrixB):
-    aRow = matrixA.numRows()
-    aCol = matrixA.numCols()
-    bRow = matrixB.numRows()
-    bCol = matrixB.numCols()
+    aRow, aCol = matrixA.numRows(), matrixA.numCols()
+    bRow, bCol = matrixB.numRows(), matrixB.numCols()
     assert aCol == bRow
     res = myArray2D(aRow, bCol)
     for i in range(aRow):
@@ -71,36 +66,38 @@ def multiply(matrixA, matrixB):
 
 
 def printMatrix(matrix):
-    numRow = matrix.numRows()
-    numCol = matrix.numCols()
+    numRow, numCol = matrix.numRows(), matrix.numCols()
     lst = [matrix[i, j] for i in range(numRow) for j in range(numCol)]
     for i in range(0, len(lst), numCol):
         print(lst[i : i + numCol])
 
 
-def toSet(matrix):
-    numRow = matrix.numRows()
-    numCol = matrix.numCols()
+# load each entry in a matrix to a list
+def matrixToList(matrix):
+    numRow, numCol = matrix.numRows(), matrix.numCols()
     entry_lst = [matrix[i, j] for i in range(numRow) for j in range(numCol)]
+    return entry_lst
+
+
+# add the values in a list to a set with a binary search implement
+def toSet(lst):
     newSet = binarySet()
-    for i in range(len(entry_lst)):
-        newSet.add(entry_lst[i])
+    for entry in lst:
+        newSet.add(entry)
     return newSet._theElements
 
 
-def toLinearSet(matrix):
-    numRow = matrix.numRows()
-    numCol = matrix.numCols()
-    entry_lst = [matrix[i, j] for i in range(numRow) for j in range(numCol)]
+# add the values in a list to a set with a linear search implement
+def toLinearSet(lst):
     newSet = linearSet()
-    for i in range(len(entry_lst)):
-        newSet.add(entry_lst[i])
+    for entry in lst:
+        newSet.add(entry)
     return newSet._theElements
 
 
+# transform 2D array to a sparse matrix with implement of 2D array (removing zero entry)
 def toSparseMatrix(matrix):
-    numRow = matrix.numRows()
-    numCol = matrix.numCols()
+    numRow, numCol = matrix.numRows(), matrix.numCols()
     newSparseMatrix = SparseMatrix(numRow, numCol)
     for i in range(numRow):
         for j in range(numCol):
@@ -108,23 +105,15 @@ def toSparseMatrix(matrix):
     return newSparseMatrix
 
 
+# retrieve the value in the sparse metrix and return a list showing the row, column, value
 def printSparseMatrix(sparseMx):
-    for i in range(len(sparseMx._elementList)):
-        print(
-            sparseMx._elementList[i].row,
-            sparseMx._elementList[i].col,
-            sparseMx._elementList[i].value,
-        )
+    sparseMatrixLst = []
+    for entry in sparseMx._elementList:
+        sparseMatrixLst.append([entry.row, entry.col, entry.value])
+    print(sparseMatrixLst)
 
 
-def bubbleSort(lst):
-    for i in range(len(lst) - 1):
-        for j in range(len(lst) - 1 - i):
-            if lst[j] > lst[j + 1]:
-                lst[j], lst[j + 1] = lst[j + 1], lst[j]
-    return lst
-
-
+#  sort a list by bubble sorting and return the swap count for sorting the list
 def swapCountBubble(lst):
     swap_count = 0
     for i in range(len(lst) - 1):
@@ -136,24 +125,34 @@ def swapCountBubble(lst):
 
 
 # Q1:
-print(findValueRowMajor(data, 70))
-print(findValueColMajor(data, 65))
+data = readfile("mA.txt")
+print("Row-major index 70:", findValueRowMajor(data, 70))
+print("Column-major index 65: ", findValueColMajor(data, 65))
 
 # Q2:
 matrixA = loadMatrix(data)
 matrixB = transpose(data)
 matrix = multiply(matrixA, matrixB)
+print("Multiply matrix A and its transpose matrix:")
 printMatrix(matrix)
 
 # Q3:
-print(toSet(matrixA))
-print(toLinearSet(matrixA))
+lst = matrixToList(matrixA)
+print("Store the values in matrix to a set with binary search implement: ", toSet(lst))
+print(
+    "Stor the values in matrix to a set with linear search implement:: ",
+    toLinearSet(lst),
+)
 
 # Q4:
 matrixSparse = toSparseMatrix(matrixA)
+print(
+    "Store the non-zeore values in a sparse matrix to a list(Format: [row index, column indx, value]): "
+)
 printSparseMatrix(matrixSparse)
 
 # Q5:
 for i in range(len(data)):
-    print(f"The {i}th row swap count: {swapCountBubble(data[i])}")
-    print(f"The {i}th row after sorted: {bubbleSort(data[i])}")
+    count = swapCountBubble(data[i])
+    print(f"The {i}th row swap count: {count}")
+    print(f"The {i}th row after sorted: {data[i]}")
